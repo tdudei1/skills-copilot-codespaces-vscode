dc5b2eb439042368fbaa26fe03ef6da53697c6d0
tdudei1 <103686976+tdudei1@users.noreply.github.com>
2025-03-10 20:27:49 +0000

    Copilot second commit P2

--- member.js
+++ member.js
@@ -1,3 +1,15 @@
 function skillsMember(){
     console.log("Skills Member");
 }
+
+function listOutFirst100FibonacciNumbers(){
+    let a = 0;
+    let b = 1;
+    let c = 0;
+    for(let i = 0; i < 100; i++){
+        console.log(a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+}
